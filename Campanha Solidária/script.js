0714d17f7eab52c05097647623072b2eae44fd39
Luiz Henrique <lbastos722@gmail.com>
2024-12-17 15:21:46 -0300

    Algumas mudanças

---
+++ Campanha Solidária/script.js
@@ -0,0 +1,20 @@
+const dropdown = document.querySelectorAll(" .sectionsButtonsSelectors .dropdown")
+const dropbtn = document.querySelectorAll(" .sectionsButtonsSelectors .dropbtn")
+const dropContentText = document.querySelectorAll(" .sectionsButtonsSelectors .dropdown-content")
+
+dropbtn.forEach((dropbtn, index) => {
+    dropbtn.addEventListener('click', () => {
+        dropdown[index].classList.toggle("ativo")
+    })
+})
+
+dropContentText.forEach((dropContentText, index) => {
+    dropContentText.addEventListener("mouseleave", () => {
+        dropdown[index].classList.remove("ativo")
+    })
+})
+
+const whatsAppLink = "https://wa.me/38991388472";
+function goToContact() {
+    window.open(whatsAppLink)
+}
