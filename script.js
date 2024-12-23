789ea6d364681fc540fd04bc321f82cdd3a9b56e
Luiz Henrique <lbastos722@gmail.com>
2024-12-23 18:34:18 -0300

    Adicionando animações e funções

--- script.js
+++ script.js
@@ -18,3 +18,25 @@ const whatsAppLink = "https://wa.me/71984771099";
 function goToContact() {
     window.open(whatsAppLink)
 }
+
+function showOverlay(id) {
+    document.getElementById(id).style.display = "flex";
+}
+
+// Fechar o overlay com o código para o Pix
+function closeOverlay(id){
+    document.getElementById(id).style.display = "none";
+}
+
+function copiarCodigo(id) {
+    const codeElement = document.getElementById(id)
+    const code = codeElement.textContent;
+
+    navigator.clipboard.writeText(code).then(() => {
+        alert("Código copiado: " + code)
+    } )
+}
+
+
+
+
